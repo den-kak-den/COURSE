480d3af6ee70f20724e88db680be30fa5f4dac6f
HDen <dzianis.havanski@gmail.com>
2025-06-05 16:33:41 +0300

    Add start types of data

--- 1.Data types.py
+++ 1.Data types.py
@@ -21,6 +21,7 @@ str = 'fmr4o3fowpffm'
 str3 = '''jsa99sd0k'''
 bool_T = True
 bool_F = False
+None_type_var = None
 
 print(int, ' - ', type(int))
 print(float, ' - ', type(float))
@@ -28,4 +29,15 @@ print(str, ' - ', type(str))
 print(str3, ' - ', type(str3))
 print(bool_T, ' - ', type(bool_T))
 print(bool_F, ' - ', type(bool_F))
+print(None_type_var, ' - ', type(None_type_var))
+
+# 5  -  <class 'int'>
+# 5.1  -  <class 'float'>        
+# fmr4o3fowpffm  -  <class 'str'>
+# jsa99sd0k  -  <class 'str'>    
+# True  -  <class 'bool'>
+# False  -  <class 'bool'>
+# None  -  <class 'NoneType'>
+
+
 
