9dc76d106a9e11b357fc75e44dfa0ea193f58b52
HDen <dzianis.havanski@gmail.com>
2025-06-05 17:28:15 +0300

    Add file with exec prog-s and 1st prog

---
+++ Exerc_Progs_1.py
@@ -0,0 +1,20 @@
+#1. 
+
+# int_1 = input("Input 1st int number: ")
+# int_2 = input("Input 2nd int number: ")
+# string = input("Input string: ")
+# float = input("Input float number: ")
+
+# print(int_1)
+# print(int_2)
+# print(string)
+# print(float)
+
+# Input 1st int number: 1
+# Input 2nd int number: 4
+# Input string: fdsafsdf
+# Input float number: 131,5
+# 1
+# 4
+# fdsafsdf
+# 131,5
